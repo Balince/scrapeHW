bf0fa49dff4e0dcfc53302bde63065ff577a18da
Balince <mbal139@gmail.com>
2019-10-19 11:10:20 -0400

    add

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_PURPLE_URI || "mongodb://localhost/scrapeHW";
+var MONGODB_URI = process.env.MONGOLAB_PURPLE_URI || "mongodb://localhost/scrapeHW";
 mongoose.connect(MONGODB_URI);
 
 // Routes
